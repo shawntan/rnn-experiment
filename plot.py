47991b01bd4578df8ffc273187d0a1f2130b2ac0
Shawn Tan <shawn@wtf.sg>
2014-07-27 15:01:19 +0800

    Modified learning rate decay policy

--- plot.py
+++ plot.py
@@ -2,10 +2,23 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-Y = np.array([ float(l.strip()) for l in open(sys.argv[1]) ])
-X = np.arange(Y.shape[0]) 
+data = []
+min_Y, max_Y = np.inf, -np.inf
+for seq_file in sys.argv[1:]:
+	Y = np.array([ float(l.strip()) for l in open(seq_file) ])
+	X = np.arange(Y.shape[0]) 
+	min_Y,max_Y = min(np.min(Y),min_Y),max(np.max(Y),max_Y)
+	data.append((X,Y))
+
+print data
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(X,Y,'r-', linewidth=1)
-ax.axis([0,200,0,1])
+colour = ['r','b','g']
+num = 0
+for X,Y in data:
+	print Y
+	ax.plot(X,Y,'%s-'%colour[num], linewidth=1)
+	ax.axis([0,Y.shape[0],min_Y - (max_Y-min_Y)*0.1,max_Y + (max_Y-min_Y)*0.1])
+	num = (num+1)%len(colour)
+
 plt.show()
--- rae.py
+++ rae.py
@@ -27,8 +27,16 @@ def make_rae(inputs,W1_i,W1_m,b_h,i_h,b2_m,b2_i):
 				T.dot(inputs,W1_i) +\
 				b_h
 			)
+		"""
+		hidden = T.dot(hidden_1,W1_m) +\
+				 T.dot(inputs,W1_i) +\
+				 b_h
+		hidden = (hidden > 0) * hidden
+		"""
+
 		reproduction_m = T.dot(hidden,W1_m.T) + b2_m
 		reproduction_i = T.dot(hidden,W1_i.T) + b2_i
+
 		return hidden,reproduction_m,reproduction_i
 
 	[hidden_,reproduction_m_,reproduction_i_],_ = theano.scan(
@@ -86,14 +94,15 @@ def build_error(X,hidden,hidden1_reproduction,input_reproduction):
 	return input_reproduction_sqerror + hidden_reproduction_sqerror
 
 if __name__ == '__main__':
-	X,parameters,hidden,hidden1_reproduction,input_reproduction,unrolled = build_network(8,24)
+	X,parameters,hidden,hidden1_reproduction,input_reproduction,unrolled = build_network(8,64)
 	f = theano.function(
 			inputs  = [X],
 			outputs = [hidden,hidden1_reproduction,input_reproduction,unrolled]
 		)
 
 	error = build_error(X,hidden,hidden1_reproduction,input_reproduction)
-	gradients = T.grad(error,wrt=parameters)
+	cost  = error # + 1e-6*sum( T.sum(abs(p)) for p in parameters )
+	gradients = T.grad(cost,wrt=parameters)
 	
 	eps = T.dscalar('eps')
 	mu  = T.dscalar('mu')
@@ -109,10 +118,18 @@ if __name__ == '__main__':
 			outputs = error
 		)
 
+	#example = np.vstack((np.eye(8),np.eye(8)))
 	example = np.eye(8)
-	for _ in xrange(50000):
+	error = 10
+	lr = 0.0001
+	t = 0
+	while error > 0.0001:
 		np.random.shuffle(example)
-		print train(example,0.001,0.999)
+		error = train(example,lr,min(1 - 3.0/(t+5),0.999))
+		#error = train(example,lr,0)
+		print error
+		t += 1
+		
 	
 
 	np.random.shuffle(example)
